32dc74713109b16d933464940eae70d961e06cd5
Josef-miguel <migsjs77@gmail.com>
2025-08-19 11:13:16 -0300

    trabalho concluido

--- Consumindo-api/Controllers/routes.py
+++ Consumindo-api/Controllers/routes.py
@@ -1,27 +1,58 @@
-import urllib.request, json
+import urllib.request
+import json
 from flask import render_template, request, url_for, redirect
-from flask import Blueprint, render_template
-
-
 
 API_KEY = "c6f8ab23618b3fe3a31460aa5d9b8122"
+TMDB_BASE_URL = "https://api.themoviedb.org/3"
+
+def fazer_requisicao(endpoint):
+    """Função utilitária para fazer requisições à API do TMDB"""
+    url = f"{TMDB_BASE_URL}{endpoint}?api_key={API_KEY}&language=pt-BR"
+    with urllib.request.urlopen(url) as response:
+        return json.loads(response.read())
+
+def buscar_generos():
+    dados = fazer_requisicao("/genre/movie/list")
+    return {genero["id"]: genero["name"] for genero in dados["genres"]}
+
 def init_app(app):
- @app.route('/')
- def home():
-    return render_template('home.html')
+    @app.route('/')
+    def home():
+        return render_template('home.html')
 
- @app.route('/apifilmes', methods=['GET', 'POST'])
- @app.route('/apifilmes/<int:id>', methods=['GET', 'POST'])
- def apifilmes(id=None):
-    urlAPI = f'https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}&language=pt-BR'
-    response = urllib.request.urlopen(urlAPI)
-    apiData = response.read()
-    listaFilmes = json.loads(apiData)["results"]
+    @app.route('/apifilmes', methods=['GET', 'POST'])
+    @app.route('/apifilmes/<int:id>', methods=['GET', 'POST'])
+    def apifilmes(id=None):
+        if id:
+   
+            filmeInfo = fazer_requisicao(f"/movie/{id}")
 
-    if id:
-        urlAPI_detalhe = f'https://api.themoviedb.org/3/movie/{id}?api_key={API_KEY}&language=pt-BR'
-        responseDetalhe = urllib.request.urlopen(urlAPI_detalhe)
-        filmeInfo = json.loads(responseDetalhe.read())
-        return render_template('filmeinfo.html', filmeInfo=filmeInfo)
+            
+            creditsData = fazer_requisicao(f"/movie/{id}/credits")
+            diretor = next((p for p in creditsData["crew"] if p["job"] == "Director"), None)
 
-    return render_template('apifilmes.html', listaFilmes=listaFilmes)
+          
+            videosData = fazer_requisicao(f"/movie/{id}/videos")
+            trailer = next((v for v in videosData["results"]
+                            if v["type"] == "Trailer" and v["site"] == "YouTube"), None)
+
+            return render_template("filmeinfo.html", filmeInfo=filmeInfo, diretor=diretor, trailer=trailer)
+
+        
+        generos_dict = buscar_generos()
+        listaFilmes = []
+
+
+        for pagina in range(1, 2):
+            endpoint = f"/movie/popular&page={pagina}"
+            url = f"{TMDB_BASE_URL}/movie/popular?api_key={API_KEY}&language=pt-BR&page={pagina}"
+            with urllib.request.urlopen(url) as response:
+                apiData = json.loads(response.read())
+                filmes = apiData["results"]
+
+                for filme in filmes:
+                    ids_generos = filme.get("genre_ids", [])
+                    filme["genres"] = [generos_dict.get(id_gen, "Desconhecido") for id_gen in ids_generos]
+                    listaFilmes.append(filme)
+
+        return render_template("apifilmes.html", listaFilmes=listaFilmes)
